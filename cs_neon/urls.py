e90ba0bb0a384467f1fc5a875764e75ca12ce238
Adewale <lilhamad8@gmail.com>
2019-12-10 14:27:24 +0100

    added urls.py to apicore

--- cs_neon/urls.py
+++ cs_neon/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cs_neon.apicore import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('apicore.urls')),
 ]
