0f2e6beb550b62dc703cce6fea7919546a539dde
Adewale <lilhamad8@gmail.com>
2019-12-10 14:50:51 +0100

    added login endup

--- apicore/urls.py
+++ apicore/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from rest_framework.authtoken.views import obtain_auth_token 
 from . import views
 
 urlpatterns = [
     path('', views.AllUsers.as_view(), name='users'),
+    path('login/', obtain_auth_token, name='login'),
 ]
